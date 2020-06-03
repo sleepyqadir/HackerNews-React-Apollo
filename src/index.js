9fba54de94340267de609d295bbab9c4461fe5fe
AQadir64 <a.qadir67@outlook.com>
2020-06-03 17:01:33 +0500

    react-apollo client

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Styles/index.css";
+import App from "./Components/App";
+import * as serviceWorker from "./serviceWorker";
+import { ApolloClient, InMemoryCache, gql } from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { createHttpLink } from "apollo-link-http";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+ReactDOM.render(
+  <Router>
+    {" "}
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </Router>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
